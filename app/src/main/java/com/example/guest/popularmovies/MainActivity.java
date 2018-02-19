34ebfe954291c4adf997eebdef6facfc8bb2b337
l1maginaire <l1maginaire00@gmail.com>
2018-02-19 17:22:32 +0200

    Storing API key

--- app/src/main/java/com/example/guest/popularmovies/MainActivity.java
+++ app/src/main/java/com/example/guest/popularmovies/MainActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String API_KEY = BuildConfig.API_KEY;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
