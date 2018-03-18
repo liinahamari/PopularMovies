02beac82af3360fd4ffde2a06440ef10f5529bf5
l1maginaire <l1maginaire00@gmail.com>
2018-03-18 12:29:54 +0200

    Cleaning

--- app/src/main/java/com/example/guest/popularmovies/base/BasePresenter.java
+++ app/src/main/java/com/example/guest/popularmovies/base/BasePresenter.java
@@ -4,31 +4,13 @@ package com.example.guest.popularmovies.base;
  * Created by l1maginaire on 3/1/18.
  */
 
-import com.example.guest.popularmovies.mvp.model.MoviesArray;
-
 import javax.inject.Inject;
 
-import io.reactivex.Observable;
-import io.reactivex.android.schedulers.AndroidSchedulers;
-import io.reactivex.disposables.Disposable;
-import io.reactivex.functions.Action;
-import io.reactivex.functions.Consumer;
-import io.reactivex.schedulers.Schedulers;
-
 public class BasePresenter<V extends BaseView> {
-
     @Inject
     protected V view;
 
     protected V getView() {
         return view;
-    }
-
-    protected Disposable subscribe(Observable<MoviesArray> observable, io.reactivex.functions.Consumer<MoviesArray> onNext,
-                   Consumer<Throwable> onError, Action onComplete) {
-        return observable
-                .subscribeOn(Schedulers.io())
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(onNext, onError, onComplete);
     }
 }
