7f4490a63592d1e7742393db35ddb97a46e3e37f
l1maginaire <l1maginaire00@gmail.com>
2018-03-27 14:09:29 +0300

    Adding addMovie and getSavedMovies methods

---
+++ app/src/main/java/com/example/guest/popularmovies/db/MoviesDbHelper.java
@@ -0,0 +1,118 @@
+package com.example.guest.popularmovies.db;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.SQLException;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+import com.example.guest.popularmovies.db.MoviesContract.Entry;
+import com.example.guest.popularmovies.mvp.model.SingleMovie;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static com.example.guest.popularmovies.db.MoviesContract.Entry.*;
+
+/**
+ * Created by l1maginaire on 3/22/18.
+ */
+
+public class MoviesDbHelper extends SQLiteOpenHelper {
+    private static final String TAG = MoviesDbHelper.class.getSimpleName();
+
+    private static final String DATABASE_NAME = "movdb.db";
+    private static final int DATABASE_VERSION = 1;
+
+    public MoviesDbHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    public void addMovie(SingleMovie movie) {
+        SQLiteDatabase db = getWritableDatabase();
+
+        ContentValues values = new ContentValues();
+        values.put(MOV_ID, movie.getTitle());
+        values.put(BACKDROP_PATH, movie.getBackdropPath());
+//        values.put(GENRE_IDS, movie.getGenreIds());
+//        values.put(IN_FAVORITES, true); //todo
+        values.put(ORIGINAL_TITLE, movie.getOriginalTitle());
+        values.put(OVERVIEW, movie.getOverview());
+        values.put(POPULARITY, movie.getPopularity());
+        values.put(POSTER_PATH, movie.getPosterPath());
+        values.put(RELEASE_DATE, movie.getReleaseDate());
+        values.put(VOTE_AVERAGE, movie.getVoteAverage());
+        values.put(VOTE_COUNT, movie.getVoteCount());
+
+        try {
+            db.insert(TABLE_NAME, null, values);
+        } catch (SQLException e) {
+            Log.d(TAG, e.getMessage());
+        }
+        db.close();
+    }
+
+
+    public List<SingleMovie> getSavedCakes() {
+        List<SingleMovie> movies = new ArrayList<>();
+        SQLiteDatabase db = this.getWritableDatabase();
+
+        try {
+            Cursor cursor = db.rawQuery("SELECT * FROM " + TABLE_NAME, null);
+            if (cursor != null) {
+                if (cursor.getCount() > 0) {
+                    if (cursor.moveToFirst()) {
+                        do {
+                            SingleMovie movie = new SingleMovie();
+                            movie.setId(Integer.valueOf(cursor.getString(cursor.getColumnIndex(MOV_ID)))); //todo catch error
+                            movie.setBackdropPath(cursor.getString(cursor.getColumnIndex(BACKDROP_PATH)));
+                            movie.setOriginalTitle(cursor.getString(cursor.getColumnIndex(ORIGINAL_TITLE)));
+                            movie.setOverview(cursor.getString(cursor.getColumnIndex(OVERVIEW)));
+                            movie.setPopularity(Double.valueOf(cursor.getString(cursor.getColumnIndex(POPULARITY))));
+                            movie.setPosterPath(cursor.getString(cursor.getColumnIndex(POSTER_PATH)));
+                            movie.setReleaseDate(cursor.getString(cursor.getColumnIndex(RELEASE_DATE)));
+                            movie.setTitle(cursor.getString(cursor.getColumnIndex(TITLE)));
+                            movie.setVoteAverage(Double.valueOf(cursor.getString(cursor.getColumnIndex(VOTE_AVERAGE))));
+                            movie.setVoteCount(Integer.valueOf(cursor.getString(cursor.getColumnIndex(VOTE_COUNT))));
+
+                            movies.add(movie);
+
+                        } while (cursor.moveToNext());
+                    }
+                }
+            }
+        } catch (SQLException e) {
+            Log.d(TAG, e.getMessage());
+        }
+        return movies;
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) { //todo CHECK NOT NULL
+        final String SQL_CREATE_WAITLIST_TABLE = "CREATE TABLE " + Entry.TABLE_NAME + " (" +
+                Entry._ID + " INTEGER PRIMARY KEY AUTOINCREMENT," +
+//                Entry._ID + " INTEGER NOT NULL," +
+                MOV_ID + " INTEGER NOT NULL," +
+                BACKDROP_PATH + " TEXT NOT NULL, " +
+                GENRE_IDS + " TEXT NOT NULL, " +
+//                IN_FAVORITES + " INTEGER NOT NULL, " +
+                ORIGINAL_TITLE + " TEXT NOT NULL, " +
+                OVERVIEW + " TEXT NOT NULL, " +
+                POPULARITY + " DOUBLE NOT NULL, " +
+                POSTER_PATH + " TEXT NOT NULL, " +
+                RELEASE_DATE + " TEXT NOT NULL, " +
+                TITLE + " TEXT NOT NULL, " +
+                VOTE_AVERAGE + " DOUBLE NOT NULL, " +
+                VOTE_COUNT + " INTEGER NOT NULL" +
+                "); ";
+        sqLiteDatabase.execSQL(SQL_CREATE_WAITLIST_TABLE);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+        sqLiteDatabase.execSQL("DROP TABLE IF EXISTS " + Entry.TABLE_NAME);
+        onCreate(sqLiteDatabase);
+    }
+}
